8070bcdd27a6f76ffa08084026edaee13ea8470e
L3viathan <git@l3vi.de>
2024-02-14 13:08:09 +0100

    Switch to sanic (closes #1)

--- app.py
+++ app.py
@@ -1,206 +1,181 @@
-from urllib.parse import parse_qs
-from robyn import Robyn
-from robyn.templating import JinjaTemplate
+import string
+from sanic import Sanic, file, html
 from database import Runbook, Section, Item, Run, Target
 
 
-app = Robyn(__file__)
-T = JinjaTemplate(".")
+app = Sanic("listen")
+with open("index.html") as f:
+    INDEX = string.Template(f.read())
 
 
-@app.get("/", const=True)
+@app.get("/")
 async def index(request):
-    return T.render_template("index.html", autoload="/runbooks")
+    return html(INDEX.substitute(autoload="/runbooks"))
 
 
-@app.get("/_/runbooks/:runbook_id")
-async def direct_runbook(request):
-    runbook_id = int(request.path_params.get("runbook_id"))
-    return T.render_template("index.html", autoload=f"/runbooks/{runbook_id}")
+@app.get("/_/runbooks/<runbook_id>")
+async def direct_runbook(request, runbook_id: int):
+    return html(INDEX.substitute(autoload=f"/runbooks/{runbook_id}"))
 
 
-@app.get("/_/runs/:run_id")
-async def direct_run(request):
+@app.get("/_/runs/<run_id>")
+async def direct_run(request, run_id: int):
     run_id = int(request.path_params.get("run_id"))
-    return T.render_template("index.html", autoload=f"/runs/{run_id}")
+    return html(INDEX.substitute(autoload=f"/runs/{run_id}"))
 
 
 @app.get("/runbooks")
 async def list_runbooks(request):
-    return f"""
+    return html(f"""
         {"\n\n".join(f"{lst:link}" for lst in Runbook.all())}
         {Runbook.new_runbook_input()}
-    """
+    """)
 
 
 @app.get("/runs")
 async def list_runs(request):
-    return "\n\n".join(f"{lst:link}" for lst in Run.all())
+    return html("\n\n".join(f"{lst:link}" for lst in Run.all()))
 
 
-@app.get("/runbooks/:runbook_id")
-async def view_runbook(request):
-    runbook = Runbook.from_id(int(request.path_params.get("runbook_id")))
-    return f"{runbook:detail}"
+@app.get("/runbooks/<runbook_id>")
+async def view_runbook(request, runbook_id: int):
+    runbook = Runbook.from_id(runbook_id)
+    return html(f"{runbook:detail}")
 
 
-@app.post("/items/new/:section_id")
-async def new_item(request):
-    (name,) = parse_qs(request.body)["name"]
-    section_id = int(request.path_params.get("section_id"))
+@app.post("/items/new/<section_id>")
+async def new_item(request, section_id: int):
+    name = request.form.get("q")
+    section_id = int(section_id)
     item = Item.create(name=name, section_id=section_id)
-    return f"""
+    return html(f"""
         {item:detail}
         {Section.new_item_input(section_id)}
-    """
+    """)
 
 
-@app.post("/items/toggle/:item_id")
-async def toggle_item(request):
-    item = Item.from_id(int(request.path_params.get("item_id")))
+@app.post("/items/toggle/<item_id>")
+async def toggle_item(request, item_id: int):
+    item = Item.from_id(item_id)
     item.toggle()
-    return f"{item:detail}"
+    return html(f"{item:detail}")
 
 
-@app.post("/items/change/:item_id")
-async def change_item(request):
-    item = Item.from_id(int(request.path_params.get("item_id")))
-    body = parse_qs(request.body)
-    names = body.get("name")
-    if not names or not names[0]:
+@app.post("/items/change/<item_id>")
+async def change_item(request, item_id: int):
+    item = Item.from_id(item_id)
+    name = request.form.get("name")
+    if not name:
         item.delete()
-        return ""
+        return html("")
     else:
-        name = names[0]
         item.rename(name)
-        return f"{item:detail}"
+        return html(f"{item:detail}")
 
 
-@app.post("/sections/change/:section_id")
-async def change_section(request):
-    section = Section.from_id(int(request.path_params.get("section_id")))
-    body = parse_qs(request.body)
-    names = body.get("name")
-    if not names or not names[0]:
+@app.post("/sections/change/<section_id>")
+async def change_section(request, section_id: int):
+    section = Section.from_id(section_id)
+    name = request.form.get("name")
+    if not name:
         section.delete()
-        return ""
+        return html("")
     else:
-        name = names[0]
         section.rename(name)
-        return f"{section:detail}"
+        return html(f"{section:detail}")
 
 
-@app.post("/runbooks/change/:runbook_id")
-async def change_runbook(request):
-    runbook = Runbook.from_id(int(request.path_params.get("runbook_id")))
-    body = parse_qs(request.body)
-    names = body.get("name")
-    if names and names[0]:
-        name = names[0]
+@app.post("/runbooks/change/<runbook_id>")
+async def change_runbook(request, runbook_id: int):
+    runbook = Runbook.from_id(runbook_id)
+    name = request.form.get("name")
+    if name:
         runbook.rename(name)
-    return f"{runbook:heading}"
+    return html(f"{runbook:heading}")
 
 
-@app.post("/runs/change/:run_id")
-async def change_run(request):
-    run = Run.from_id(int(request.path_params.get("run_id")))
-    body = parse_qs(request.body)
-    names = body.get("name")
-    if names and names[0]:
-        name = names[0]
+@app.post("/runs/change/<run_id>")
+async def change_run(request, run_id: int):
+    run = Run.from_id(run_id)
+    name = request.form.get("name")
+    if name:
         run.rename(name)
-    return f"{run:heading}"
+    return html(f"{run:heading}")
 
 
-@app.post("/sections/new/:runbook_id")
-async def new_section(request):
-    (name,) = parse_qs(request.body)["name"]
-    runbook_id = int(request.path_params.get("runbook_id"))
+@app.post("/sections/new/<runbook_id>")
+async def new_section(request, runbook_id: int):
+    name = request.form.get("name")
     section = Section.create(name=name, runbook_id=runbook_id)
-    return f"""
+    return html(f"""
         {section:detail}
         {Runbook.new_section_input(runbook_id)}
-    """
+    """)
 
 
-@app.post("/runs/new/:runbook_id")
-async def new_run(request):
-    (name,) = parse_qs(request.body)["name"]
-    runbook_id = int(request.path_params.get("runbook_id"))
+@app.post("/runs/new/<runbook_id>")
+async def new_run(request, runbook_id: int):
+    name = request.form.get("name")
     run = Run.create(runbook_id=runbook_id, name=name)
-    return f"""
+    return html(f"""
         <li>{run:link}</li>
         {Runbook.new_section_input(runbook_id)}
-    """
+    """)
 
 
 @app.post("/runbooks/new")
 async def new_runbook(request):
-    (name,) = parse_qs(request.body)["name"]
+    name = request.form.get("name")
     runbook = Runbook.create(name=name)
-    return f"""
+    return html(f"""
         {runbook:link}
         {Runbook.new_runbook_input()}
-    """
+    """)
 
 
-@app.get("/runs/:run_id")
-async def view_run(request):
-    run = Run.from_id(int(request.path_params.get("run_id")))
-    return f"{run:detail}"
+@app.get("/runs/<run_id>")
+async def view_run(request, run_id: int):
+    run = Run.from_id(run_id)
+    return html(f"{run:detail}")
 
 
-@app.post("/checkmarks/check/:run_id/:item_id")
-async def check_checkmark(request):
-    item = Item.from_id(int(request.path_params.get("item_id")))
-    run = Run.from_id(int(request.path_params.get("run_id")))
-    return item.check_for(run)
+@app.post("/checkmarks/check/<run_id>/<item_id>")
+async def check_checkmark(request, run_id: int, item_id: int):
+    item = Item.from_id(item_id)
+    run = Run.from_id(run_id)
+    return html(item.check_for(run))
 
 
-@app.post("/checkmarks/check/:run_id/:item_id/:target_id")
-async def check_checkmark_for_target(request):
-    item = Item.from_id(int(request.path_params.get("item_id")))
-    run = Run.from_id(int(request.path_params.get("run_id")))
-    target_id = int(request.path_params.get("target_id"))
-    return item.check_for(run, target_id=target_id)
+@app.post("/checkmarks/check/<run_id>/<item_id>/<target_id>")
+async def check_checkmark_for_target(
+    request,
+    run_id: int,
+    item_id: int,
+    target_id: int,
+):
+    item = Item.from_id(item_id)
+    run = Run.from_id(run_id)
+    return html(item.check_for(run, target_id=target_id))
 
 
-@app.post("/targets/new/:run_id")
-async def new_target(request):
-    run = Run.from_id(int(request.path_params.get("run_id")))
-    (name,) = parse_qs(request.body)["name"]
+@app.post("/targets/new/<run_id>")
+async def new_target(request, run_id: int):
+    run = Run.from_id(run_id)
+    name = request.form.get("name")
     Target.create(run_id=run.id, name=name)
-    return f"{run:detail}"
+    return html(f"{run:detail}")
 
 
 @app.get("/vendor/htmx.min.js")
 async def _htmx_js(request):
-    return {
-        "file_path": "./htmx.min.js",
-        "headers": {
-            "Content-Type": "text/javascript",
-        },
-    }
+    return await file("./htmx.min.js")
 
 
 @app.get("/vendor/Satisfy-Regular.woff2")
 async def _satisfy_font(request):
-    return {
-        "file_path": "./Satisfy-Regular.woff2",
-        "headers": {
-            "Content-Type": "application/font-woff2",
-        },
-    }
+    return await file("./Satisfy-Regular.woff2")
 
 
 @app.get("/favicon.ico")
 async def _favicon(request):
-    return {
-        "file_path": "./favicon.ico",
-        "headers": {
-            "Content-Type": "image/x-icon",
-        },
-    }
-
-
-app.start(host="0.0.0.0", port=8080)
+    return await file("favicon.ico")
